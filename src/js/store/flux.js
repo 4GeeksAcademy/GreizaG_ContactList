d81231728428a55d97ab983b09cff5cbf61207da
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-08-09 17:01:46 +0000

    Modificación deleteContact para que se actualice automaticamente el store

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -28,29 +28,60 @@ const getState = ({ getStore, getActions, setStore }) => {
 					body: JSON.stringify(data),
 					headers: { "Content-Type": "application/json" }
 				})
-				.then(response => {
-					console.log(response);
-					if (response.ok) {
-						return response.json();
-					}
-					throw new Error("Ocurrió un error agregando un nuevo contacto");
-				})
-				.then((data) => {
-					console.log("Contacto creado: ", data);
-					return getAllContacts();
-				})
-				.catch((error) => console.log("Error: ", error));
+					.then(response => {
+						console.log(response);
+						if (response.ok) {
+							return response.json();
+						}
+						throw new Error("Ocurrió un error agregando un nuevo contacto");
+					})
+					.then((data) => {
+						console.log("Contacto creado: ", data);
+						return getAllContacts();
+					})
+					.catch((error) => console.log("Error: ", error));
 			},
 
+			// deleteContact: (id) => {
+
+			// 	const updateContacts = getStore().contacts.filter(
+			// 		(contact) => contact.id != id
+			// 	  );
+
+			// 	return fetch(`https://playground.4geeks.com/contact/agendas/greizag/contacts/${id}`, {
+			// 		method: "DELETE"
+			// 		, headers: { "Content-Type": "application/json" }
+			// 	})
+			// 		.then(response => {
+			// 			console.log(response)
+			// 		})
+			// 		.then(() => {
+			// 			console.log("Contacto borrado");
+			// 			console.log(contacts);
+			// 			getAllContacts();
+			// 		})
+			// 		.catch((error) => error)
+			// },
+
 			deleteContact: (id) => {
+
+				const updateContacts = getStore().contacts.filter(
+					(contact) => contact.id != id
+				);
+
 				return fetch(`https://playground.4geeks.com/contact/agendas/greizag/contacts/${id}`, {
 					method: "DELETE"
-				, headers: { "Content-Type": "application/json" }})
+					, headers: { "Content-Type": "application/json" }
+				})
 					.then(response => {
 						console.log(response)
 					})
 					.then(() => {
 						console.log("Contacto borrado");
+						console.log(getStore().contacts);
+						getStore().contacts = updateContacts;
+						console.log("Este es updateContacts: ", updateContacts);
+						setStore({ contacts: updateContacts });
 						getAllContacts();
 					})
 					.catch((error) => error)
